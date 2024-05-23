80980c306be8955707fc343aa1a9de268f822c53
raja-0007 <rajadaragani07@gmail.com>
2024-05-23 17:50:24 +0530

    index renamed

--- routers/routers.js
+++ routers/routers.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const controllers = require('../controllers/controllers')
-
+router.get('/',(req,res)=>{
+    res.send('hello')
+})
 // router.post('/paymentDetails',controllers.addPayment)
 router.post('/create-checkout-session',controllers.addPayment)
 router.get('/getTransactions',controllers.getTransactions)
